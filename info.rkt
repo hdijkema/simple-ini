c13fb6e6cb114699ea8ee8c6133162198ce58151
Hans Dijkema <hans@dijkewijk.nl>
2025-08-18 09:26:58 +0200

    ok

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define pkg-authors '(hnmdijkema))
-(define version "0.2.1")
+(define version "0.2.2")
 (define license 'Apache-2.0)
 ;(define collection "simple-ini")
 (define pkg-desc "A Simple .ini file reader/writer for racket")
